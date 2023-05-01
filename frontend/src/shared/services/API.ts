599fc3133f325e28a72cef5eee002738c667356a
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-01 11:14:23 +0900

    chore: reduxが無くても動くようにした

--- frontend/src/shared/services/API.ts
+++ frontend/src/shared/services/API.ts
@@ -1,11 +1,9 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-
 import { Hello } from '../models';
 
 const API_ENDPOINT_PATH =
   import.meta.env.VITE_API_ENDPOINT_PATH ?? '';
 
-export const getHello = createAsyncThunk<Hello>('getHello', async () => {
+export const getHello = async () => {
   const response = await fetch(`${API_ENDPOINT_PATH}/hello`);
-  return await response.json();
-});
+  return await response.json() as Hello;
+};
