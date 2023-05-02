0bc9eb8cb7c6cdf140de8b9ee4bd6eef37a53c4c
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-02 11:20:05 +0900

    feature: /posts の雛形を作成
    /posts にアクセスできるようになった

---
+++ frontend/src/features/posts/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { Posts } from './Posts';
+
+export function PostsRoutes() {
+  return (
+    <Routes>
+      <Route path="" element={<Posts />} />
+      <Route path="*" element={<Navigate to="." />} />
+    </Routes>
+  );
+}
