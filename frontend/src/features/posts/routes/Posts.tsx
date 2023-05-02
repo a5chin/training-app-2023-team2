eabca245bcb1578358d44b575ddcf9502a08e9a7
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-02 17:29:29 +0900

    feat: MainLayoutを作成し、全体共通のレイアウトを定義した

--- frontend/src/features/posts/routes/Posts.tsx
+++ frontend/src/features/posts/routes/Posts.tsx
@@ -1,35 +1,61 @@
 import { Box, Flex, Icon, Text } from '@chakra-ui/react';
+import { Posts as PostsType } from '../types';
+
+function Post() {
+  return (
+    <Flex direction="row">
+      <Flex direction="column">
+        <Icon />
+      </Flex>
+      <Flex direction="row">
+        <Text>This is a first tweet.</Text>
+        <Flex direction="column">
+          <Box>Comment</Box>
+          <Box>Like</Box>
+          <Box>Share</Box>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
 
 export function Posts() {
   // const { posts, isLoading, isError } = usePosts();
+  const posts: PostsType = [
+    {
+      id: 1,
+      title: 'Post1',
+      body: 'This is post 1.',
+      user: {
+        id: 1,
+        name: 'user1',
+      },
+    },
+    {
+      id: 2,
+      title: 'Post2',
+      body: 'This is post 2.',
+      user: {
+        id: 2,
+        name: 'user2',
+      },
+    },
+  ];
+
+  const postElements = posts.map((post) => <Post key={post.id} />);
 
   return (
-    <Flex direction="column">
-      {/* Side bar */}
-      <Flex direction="row">
-        <Box>Home</Box>
-        <Box>Profile</Box>
-      </Flex>
-
+    <Flex direction="row" w="full">
       {/* Tweets */}
-      <Flex direction="row">
-        {/* Tweet */}
-        <Flex direction="column">
-          <Flex direction="row">
-            <Icon />
-          </Flex>
-          <Flex direction="row">
-            <Text>This is a first tweet.</Text>
-            <Flex direction="column">
-              <Box>Comment</Box>
-              <Box>Like</Box>
-              <Box>Share</Box>
-            </Flex>
-          </Flex>
-        </Flex>
+      <Flex direction="column" fontSize="md" bg="blue">
+        {postElements}
       </Flex>
 
       {/* Sub information */}
+      <Flex direction="column" bg="green">
+        <Box>Ranking</Box>
+        <Box>Recommendation</Box>
+      </Flex>
     </Flex>
   );
 }
