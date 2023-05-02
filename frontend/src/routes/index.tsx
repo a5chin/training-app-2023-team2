4744dae2e21e8cfb6ce3a99f049a20fe54aa2b09
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-02 11:20:05 +0900

    feature: /posts の雛形を作成
    /posts にアクセスできるようになった

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,13 @@
 import { useRoutes, RouteObject } from 'react-router-dom';
 
 import { HelloWorld } from '@/features/helloworld';
+import { PostsRoutes } from '@/features/posts';
 
 export function AppRoutes() {
-  const routes: RouteObject[] = [{ path: '/', element: <HelloWorld /> }];
+  const routes: RouteObject[] = [
+    { path: '/', element: <HelloWorld /> },
+    { path: '/posts', element: <PostsRoutes /> },
+  ];
 
   const element = useRoutes([...routes]);
 
