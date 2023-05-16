d6050df67146ec982b886c18fd2200433aa8cb8a
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-16 10:54:09 +0900

    feat: Twitterっぽいヘッダーを作成

--- frontend/src/components/Layout/MainLayout.tsx
+++ frontend/src/components/Layout/MainLayout.tsx
@@ -1,6 +1,51 @@
-import { Box, Flex, Icon } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import { AiFillHome, AiOutlineUser } from 'react-icons/ai';
+import { GiHummingbird } from 'react-icons/gi';
+import { BiPen } from 'react-icons/bi';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+function TweetButton() {
+  const isLessThanMd = useBreakpointValue({ base: true, md: false });
+  if (isLessThanMd) {
+    return (
+      <IconButton
+        aspectRatio="1/1"
+        size="md"
+        icon={<BiPen />}
+        bg="blue.400"
+        aria-label="Tweet button"
+        rounded="full"
+      />
+    );
+  }
+  return (
+    <Button
+      bg="blue.400"
+      textColor="white"
+      fontSize="xl"
+      fontWeight="bold"
+      width="full"
+      variant="solid"
+      boxShadow="md"
+      rounded="full"
+      paddingY={6}
+      textAlign="center"
+    >
+      Tweet
+    </Button>
+  );
+}
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -9,22 +54,36 @@ type MainLayoutProps = {
 export function MainLayout({ children }: MainLayoutProps) {
   // TODO: 以下の情報を渡せるようにする
   // - SideMenuのどの項目のページを表示しているか
-  // - MainContentを渡す
-  // - SubInformationを渡す
   return (
     <Flex direction="row" w="100vw" h="100vh">
-      <Flex as="header" direction="column" bg="red" flexGrow={1}>
-        <Box>
-          <Icon as={AiFillHome} />
-          Home
-        </Box>
-        <Box>
-          <Icon as={AiOutlineUser} />
-          Profile
-        </Box>
+      <Flex
+        as="header"
+        direction="row-reverse"
+        bg="red"
+        flexGrow={1}
+        paddingX={4}
+      >
+        <Stack width={{ md: '275px' }}>
+          <Icon boxSize={8} as={GiHummingbird} />
+          <Box>
+            <HStack as={Link} to="/" spacing={4}>
+              <Icon boxSize={8} as={AiFillHome} />
+              <Text fontSize={{ base: '0', md: '3xl' }} fontWeight="medium">
+                Home
+              </Text>
+            </HStack>
+            <HStack as={Link} to="/" spacing={4}>
+              <Icon boxSize={8} as={AiOutlineUser} />
+              <Text fontSize={{ base: '0', md: '3xl' }} fontWeight="medium">
+                Profile
+              </Text>
+            </HStack>
+          </Box>
+          <TweetButton />
+        </Stack>
       </Flex>
 
-      <Box as="main" h="full" flexGrow={2} flexShrink={0}>
+      <Box as="main" h="full" flexGrow={3} flexShrink={0}>
         {children}
       </Box>
     </Flex>
