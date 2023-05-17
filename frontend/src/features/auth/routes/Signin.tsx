037a54faaa91b0f236aeeccfee29d1523d4f7282
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-17 15:31:19 +0900

    feat: Signin, Signup ページを作成

--- frontend/src/features/auth/routes/Signin.tsx
+++ frontend/src/features/auth/routes/Signin.tsx
@@ -1,3 +1,29 @@
+import { Box, Button } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useAuthContext } from '@/lib/auth';
+
 export function Signin() {
-  return <div>This is signin page</div>;
+  const { signin, currentUser } = useAuthContext();
+
+  useEffect(() => {
+    // TODO: ユーザーがログイン済みなら /posts に飛ばす
+    console.log(`currentUser has changed! ${JSON.stringify(currentUser)}`);
+  }, [currentUser]);
+
+  return (
+    <Box>
+      <Box>This is signup page.</Box>
+      <Box>currentUser: {currentUser ? currentUser.name : 'Not signined'}</Box>
+      <Button
+        onClick={async () => {
+          await signin({
+            email: 'testuser@a.com',
+            password: 'password',
+          });
+        }}
+      >
+        Signin!
+      </Button>
+    </Box>
+  );
 }
--- frontend/src/features/auth/routes/Signup.tsx
+++ frontend/src/features/auth/routes/Signup.tsx
@@ -1,3 +1,30 @@
+import { Box, Button } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useAuthContext } from '@/lib/auth';
+
 export function Signup() {
-  return <div>This is signup page</div>;
+  const { signup, currentUser } = useAuthContext();
+
+  useEffect(() => {
+    // TODO: ユーザーがログイン済みなら /posts に飛ばす
+    console.log(`currentUser has changed! ${JSON.stringify(currentUser)}`);
+  }, [currentUser]);
+
+  return (
+    <Box>
+      <Box>This is signup page.</Box>
+      <Box>currentUser: {currentUser ? currentUser.name : 'Not signined'}</Box>
+      <Button
+        onClick={async () => {
+          await signup({
+            name: 'Test User',
+            email: 'testuser@a.com',
+            password: 'password',
+          });
+        }}
+      >
+        Signup!
+      </Button>
+    </Box>
+  );
 }
