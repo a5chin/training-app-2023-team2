5ab9c5c039c5c894d72e038eac03aa0cd8c48a45
xavifrog <msato9903fjh@gmail.com>
2023-05-18 14:26:25 +0900

    feat: デザインをTwitterに寄せる

---
+++ frontend/src/features/posts/components/ReplayFormInDetail.tsx
@@ -0,0 +1,27 @@
+import { Flex, Text, FormControl, Input, Button } from '@chakra-ui/react';
+
+export function ReplayFormInDetail() {
+  return (
+    <FormControl
+      py="12px"
+      width="100%"
+      borderTopWidth={1}
+      borderColor="red.200"
+      borderStyle="solid"
+    >
+      <Text fontSize="15pt">返信先：</Text>
+      <Input
+        size="sm"
+        type="text"
+        placeholder="返信を投稿"
+        bg="inherit"
+        variant="ghost"
+      />
+      <Flex justify="right">
+        <Button size="xl" colorScheme="teal" type="submit">
+          返信
+        </Button>
+      </Flex>
+    </FormControl>
+  );
+}
--- frontend/src/features/posts/routes/PostDetail.tsx
+++ frontend/src/features/posts/routes/PostDetail.tsx
@@ -1,24 +1,69 @@
-import { Box, Flex } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+import { Box, Flex, Text, HStack } from '@chakra-ui/react';
 import { usePostDetail } from '../api/getPostDetail';
-import { Post } from '../components/Post';
+
+import { CustomBackButton } from '../components/CustomBackButton';
+import { CustomCommentButton } from '../components/CustomCommentButton';
+import { CustomGoodButton } from '../components/CustomGoodButton';
+import { ReplayFormInDetail } from '../components/ReplayFormInDetail';
 
 export function PostDetail() {
   const { postId } = useParams<{ postId: string }>();
   const { post } = usePostDetail(postId ?? '');
 
   return (
-    <Flex direction="row" w="full">
-      {/* Tweets */}
-      <Flex flexGrow={2} direction="column" fontSize="md" bg="blue">
-        {post && <Post key={post.id} post={post} />}
+    <div>
+      <Flex direction="row" w="full">
+        <Flex flexGrow={2} direction="column" fontSize="md" bg="blue">
+          <Box borderColor="black" borderWidth="1px" py={2}>
+            {post ? (
+              <div>
+                <HStack>
+                  <CustomBackButton path="/posts" />
+                  <Text fontWeight="bold">投稿</Text>
+                </HStack>
+                <Flex direction="row" px="19px">
+                  <Flex direction="column" flex="auto">
+                    <HStack>
+                      <Text>{post.title}</Text>
+                      <Text>written by {post.user.name}</Text>
+                    </HStack>
+                    <Text>{post.body}</Text>
+                    <Box
+                      width="100%"
+                      borderTopWidth={1}
+                      borderColor="red.200"
+                      borderStyle="solid"
+                    >
+                      <Text fontSize="15pt">件のいいね</Text>
+                    </Box>
+                    <HStack
+                      width="100%"
+                      borderTopWidth={1}
+                      borderColor="red.200"
+                      borderStyle="solid"
+                    >
+                      <CustomCommentButton baseColor="black" hoverColor="red" />
+                      <CustomGoodButton
+                        baseColor="black"
+                        hoverColor="pink"
+                        fillColor="pink"
+                      />
+                    </HStack>
+                    <ReplayFormInDetail />
+                  </Flex>
+                </Flex>
+              </div>
+            ) : (
+              <Text>該当の投稿はありません</Text>
+            )}
+          </Box>
+        </Flex>
+        <Flex flexGrow={1} direction="column" bg="green">
+          <Box>Ranking</Box>
+          <Box>Recommendation</Box>
+        </Flex>
       </Flex>
-
-      {/* Sub information */}
-      <Flex flexGrow={1} direction="column" bg="green">
-        <Box>Ranking</Box>
-        <Box>Recommendation</Box>
-      </Flex>
-    </Flex>
+    </div>
   );
 }
