39da890d2e177bb346982f85ce8173c7d77d118c
gari8 <hayato.hagari@gmail.com>
2023-05-19 13:41:34 +0900

    fix: rm unsued dsn

--- backend/infrastructure/driver/database.go
+++ backend/infrastructure/driver/database.go
@@ -23,7 +23,7 @@ func NewDB(conf *config.Config) *gorm.DB {
 	dsn := fmt.Sprintf("%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, host, port, dbname)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println(dsn, err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	db = db.Debug()
