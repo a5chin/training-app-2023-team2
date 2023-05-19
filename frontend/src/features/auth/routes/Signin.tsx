be5f2f0dd3a7d3824e549a716871c25fbe7bb8b5
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-19 13:25:51 +0900

    feat: /auth/signin ページを作成

--- frontend/src/features/auth/routes/Signin.tsx
+++ frontend/src/features/auth/routes/Signin.tsx
@@ -1,36 +1,102 @@
-import { Box, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 import { useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@/lib/auth';
 
+type UserInput = {
+  email: string;
+  password: string;
+};
+
 export function Signin() {
-  const { signin, signout, currentUser } = useAuth();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<UserInput>({ mode: 'onBlur' });
+  const { signin, currentUser } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // TODO: ユーザーがログイン済みなら /posts に飛ばす
-    console.log(`currentUser has changed! ${JSON.stringify(currentUser)}`);
-  }, [currentUser]);
+    // ユーザーがログイン済みなら / に飛ばす
+    if (currentUser) {
+      navigate('/');
+    }
+  }, [currentUser, navigate]);
+
+  const onSubmit: SubmitHandler<UserInput> = async (userInput) => {
+    console.log(`UserInput: ${JSON.stringify(userInput)}`);
+    // TODO: エラーレスポンスのハンドリング
+    await signin(userInput);
+  };
 
   return (
-    <Box>
-      <Box>This is signin page.</Box>
-      <Box>currentUser: {currentUser ? currentUser.name : 'Not signined'}</Box>
-      <Button
-        onClick={async () => {
-          await signin({
-            email: 'testuser@a.com',
-            password: 'password',
-          });
-        }}
-      >
-        Signin!
-      </Button>
-      <Button
-        onClick={async () => {
-          await signout();
-        }}
-      >
-        Signout!
-      </Button>
+    <Box padding={4}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={!!(errors.email || errors.password)}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            id="email"
+            placeholder="email"
+            type="email"
+            {...register('email', {
+              required: {
+                value: true,
+                message: 'メールアドレスを入力してください',
+              },
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                message: 'メールアドレスの形式が合っていません。',
+              },
+            })}
+          />
+          <FormErrorMessage>
+            {errors.email && <span>{errors.email?.message}</span>}
+          </FormErrorMessage>
+
+          <FormLabel htmlFor="password">password</FormLabel>
+          <Input
+            id="password"
+            placeholder="password"
+            type="password"
+            {...register('password', {
+              required: {
+                value: true,
+                message: 'パスワードを入力してください',
+              },
+              minLength: {
+                value: 8,
+                message: 'パスワードは8文字以上である必要があります',
+              },
+              maxLength: {
+                value: 100,
+                message: 'パスワードは100文字以下である必要があります',
+              },
+            })}
+          />
+          <FormErrorMessage>
+            {errors.password && <span>{errors.password?.message}</span>}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          bg="blue.400"
+          colorScheme="teal"
+          isLoading={isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 }
