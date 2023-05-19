896b28bb6f35fee1a55819489d6bd4ebab82004d
Junichi Sasaki <junichi.sasaki@dena.com>
2023-05-19 18:00:00 +0900

    feat: Signoutページへのルーティングを追加

--- frontend/src/features/auth/routes/index.tsx
+++ frontend/src/features/auth/routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Signin } from './Signin';
 import { Signup } from './Signup';
+import { Signout } from './Signout';
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="signin" element={<Signin />} />
       <Route path="signup" element={<Signup />} />
+      <Route path="signout" element={<Signout />} />
     </Routes>
   );
 }
