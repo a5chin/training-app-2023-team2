77a75a646c1ee6d884539d0558e82bd5e74f4585
xavifrog <msato9903fjh@gmail.com>
2023-05-21 20:23:34 +0900

    fix: add isLoading and mutate in useAspidaSWR

--- frontend/src/features/posts/api/getPosts.ts
+++ frontend/src/features/posts/api/getPosts.ts
@@ -4,10 +4,15 @@ import { aspidaClient } from '@/lib/aspida';
 export const usePosts = () => {
   console.log(aspidaClient.posts);
   // TODO: ページング対応
-  const { data, error } = useAspidaSWR(aspidaClient.posts, 'get');
+  const { data, error, isLoading, mutate } = useAspidaSWR(
+    aspidaClient.posts,
+    'get'
+  );
 
   return {
     posts: data?.body.posts,
+    isLoading,
     isError: error,
+    mutate,
   };
 };
