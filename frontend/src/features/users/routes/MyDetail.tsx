b5cb3011d02c27e29cadc672efe058076e233977
a5chin <eigo.hara@dena.jp>
2023-05-24 15:57:36 +0900

    feature: add icon for MyDetail

--- frontend/src/features/users/routes/MyDetail.tsx
+++ frontend/src/features/users/routes/MyDetail.tsx
@@ -1,3 +1,19 @@
-export function MyDetail() {
-  return <div>MyDetail</div>;
+import { Box, Stack } from '@chakra-ui/react';
+import { UserIcon } from '@/components/Avatar/BoringAvatar';
+import { User } from '../types';
+
+type MyDetailProps = {
+  currentUser: User | null;
+};
+
+export function MyDetail({ currentUser }: MyDetailProps) {
+  return (
+    <Stack>
+      {currentUser ? (
+        <UserIcon name={currentUser.name} />
+      ) : (
+        <Box>User has not logged in!</Box>
+      )}
+    </Stack>
+  );
 }
