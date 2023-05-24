34fcac6449ca4342642d0af1b5eb8188dab6daf6
xavifrog <msato9903fjh@gmail.com>
2023-05-24 21:59:53 +0900

    feat: random user recommend

--- recommend/main.py
+++ recommend/main.py
@@ -1,45 +1,48 @@
 from fastapi import FastAPI
 from transformers import AutoTokenizer, AutoModel
+import random
 import requests
 import json
 
 app = FastAPI()
-tokenizer = AutoTokenizer.from_pretrained("line-corporation/line-distilbert-base-japanese", trust_remote_code=True)
-model = AutoModel.from_pretrained("line-corporation/line-distilbert-base-japanese")
+# tokenizer = AutoTokenizer.from_pretrained("line-corporation/line-distilbert-base-japanese", trust_remote_code=True)
+# model = AutoModel.from_pretrained("line-corporation/line-distilbert-base-japanese")
 
-sentence = ["LINE株式会社で[MASK]の研究・開発をしている。", "LINE株式会社で[MASK]の研究・開発をしている。"]
+app.user_info = None
+app.profile = None
+app.last_hidden_state = None
+app.sample_size = 3
 
-def pirnt_test():
-    print(model(**tokenizer(sentence, return_tensors="pt")).last_hidden_state.shape)
+def update_vec():
+    res = requests.get("http://backend:9000/api/v1/users")
+    values = json.loads(res.text)
 
-# def update_vec():
-#     res = requests.get("http://localhost:9000/api/v1/users")
-#     values = json.loads(res.text)
-#     profiles = [v.profile for v in values]
-#     last_hidden_state = model(**tokenizer(sentence, return_tensors="pt")).last_hidden_state
+    print(values)
+    user_info = [{"id": v["id"], "name": v["name"]} for v in values]
+    profiles = [v["profile"] for v in values]
+    # last_hidden_state = model(**tokenizer(profiles, return_tensors="pt", padding='max_length', max_length=32)).last_hidden_state[:, 0]
+    last_hidden_state=None
 
-#     return last_hidden_state[:,0]
+    return user_info, profiles, last_hidden_state
 
-# user_vec = update_vec()
+# app.user_info, app.profile, app.last_hidden_state = update_vec()
 
+@app.get("/update")
+def update():
+    # try:
+    app.user_info, app.profile, app.last_hidden_state = update_vec()
+    return {"status": "success"}
+    # except:
+    #     return  {"status": "error"}
+    
+@app.get("/{post_id}")
+def get_recommend():
+    #random.sample(app.user_info, app.sample_size if len(app.user_info)>(app.sample_size) else len(app.user_info))
+    if app.user_info!=None:
+        return {"data": app.user_info}
+    else:
+        return {"data": "error"}
+    
 @app.get("/")
 def read_root():
-    pirnt_test()
     return {"Hello": "World"}
-
-# @app.get("/{post_id}")
-# def get_recommend():
-#     pirnt_test()
-#     return {"Hello": "World"}
-
-# @app.get("/update")
-# def update_vec():
-#     try:
-#         user_vec = update_vec()
-#         return {"status": "success"}
-#     except:
-#         return  {"status": "error"}
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str = None):
-#     return {"item_id": item_id, "q": q}
