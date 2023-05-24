ef336b7d27c18367f9e984522d75f2bedba43c56
a5chin <eigo.hara@dena.jp>
2023-05-24 15:57:36 +0900

    feature: add icon for MyDetail

--- frontend/src/features/users/routes/index.tsx
+++ frontend/src/features/users/routes/index.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import { useAuth } from '@/lib/auth';
 import { UserDetail } from './UserDetail';
 import { MyDetail } from './MyDetail';
 
 export function UsersRoutes() {
+  const { currentUser } = useAuth();
+
   return (
     <Routes>
       <Route path=":id" element={<UserDetail />} />
-      <Route path="me/profile" element={<MyDetail />} />
+      <Route
+        path="me/profile"
+        element={<MyDetail currentUser={currentUser} />}
+      />
     </Routes>
   );
 }
