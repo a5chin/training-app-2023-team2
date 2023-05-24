7d57f606429b3d5700ecdb7f7e8537b6f607c296
a5chin <eigo.hara@dena.jp>
2023-05-24 19:01:32 +0900

    fix: useAuth

--- frontend/src/features/users/routes/index.tsx
+++ frontend/src/features/users/routes/index.tsx
@@ -1,18 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { useAuth } from '@/lib/auth';
 import { UserDetail } from './UserDetail';
 import { MyDetail } from './MyDetail';
 
 export function UsersRoutes() {
-  const { currentUser } = useAuth();
-
   return (
     <Routes>
       <Route path=":id" element={<UserDetail />} />
-      <Route
-        path="me/profile"
-        element={<MyDetail currentUser={currentUser} />}
-      />
+      <Route path="me/profile" element={<MyDetail />} />
     </Routes>
   );
 }
