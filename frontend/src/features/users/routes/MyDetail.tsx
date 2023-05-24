30b643c70d7b8fcdea8c5ceb78a268b36f047d85
a5chin <eigo.hara@dena.jp>
2023-05-24 17:53:40 +0900

    feature: add icon for MyDetailPage

--- frontend/src/features/users/routes/MyDetail.tsx
+++ frontend/src/features/users/routes/MyDetail.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@chakra-ui/react';
+import { Box, Stack, Image, Button, HStack, Text } from '@chakra-ui/react';
 import { UserIcon } from '@/components/Avatar/BoringAvatar';
 import { User } from '../types';
 
@@ -9,11 +9,28 @@ type MyDetailProps = {
 export function MyDetail({ currentUser }: MyDetailProps) {
   return (
     <Stack>
+      <Image
+        src="https://picsum.photos/800/200"
+        objectFit="cover"
+        alt="header"
+      />
       {currentUser ? (
-        <UserIcon name={currentUser.name} />
+        <Box>
+          <Box paddingX={5} marginTop="-8%">
+            <UserIcon name={currentUser.name} size="10%" />
+          </Box>
+        </Box>
       ) : (
         <Box>User has not logged in!</Box>
       )}
+      <HStack justifyContent="space-between" padding="10">
+        <Text fontSize="3xl" fontWeight="medium">
+          {currentUser && currentUser.name}
+        </Text>
+        <Button colorScheme="twitter" rounded="full" alignSelf="end">
+          プロフィールを入力
+        </Button>
+      </HStack>
     </Stack>
   );
 }
