0d615638a2562e2e681706109c1aacdb7991efa9
gari8 <hayato.hagari@gmail.com>
2023-05-24 10:37:59 +0900

    fix: cannot reply logoutUser

--- frontend/src/features/posts/components/ReplyModal.tsx
+++ frontend/src/features/posts/components/ReplyModal.tsx
@@ -27,6 +27,7 @@ import { axios } from '@/lib/axios';
 import { Post } from '@/features/posts';
 import { aspidaClient } from '@/lib/aspida';
 import { UserIcon } from '@/components/Avatar/BoringAvatar';
+import { useAuth } from '@/lib/auth';
 
 type DisclosureType = {
   isOpen: boolean;
@@ -59,6 +60,7 @@ export function ReplyModal({ disclosure, post }: Props) {
   const { colorMode } = useColorMode();
   const navigate = useNavigate();
   const toast = useToast();
+  const { currentUser } = useAuth();
   const [loading, { on: onLoading, off: offLoading }] = useBoolean(false);
 
   const handleReply = useCallback(async () => {
@@ -85,86 +87,90 @@ export function ReplyModal({ disclosure, post }: Props) {
   }, [getValues, navigate, offLoading, onLoading, post.id, reset, toast]);
 
   return (
-    <Modal {...disclosure} colorScheme="twitter">
-      <ModalOverlay />
-      <ModalContent>
-        <form onSubmit={handleSubmit(handleReply)}>
-          <ModalHeader fontSize="25px" color="white" ml={0}>
-            <HStack justifyContent="start">
-              <IconButton
-                icon={<CloseIcon />}
-                size="sm"
-                color="gray"
-                rounded={100}
-                aria-label="close-button"
-                onClick={disclosure.onClose}
-              />
-            </HStack>
-          </ModalHeader>
-          <ModalBody pb={6}>
-            <Stack>
-              <HStack alignItems="start">
-                <Stack>
-                  {post.user && <UserIcon name={post.user.name} />}
-                  <Center h="50px">
-                    <Divider
-                      orientation="vertical"
-                      size="lg"
-                      colorScheme="twitter"
-                    />
-                  </Center>
-                </Stack>
-                <Stack>
-                  <Stack px={1} fontSize="sm">
-                    <Text fontWeight="semibold" fontSize="md">
-                      {post.user?.name}
-                    </Text>
-                    <Text fontSize="sm">{post.body}</Text>
-                    {post.user && (
-                      <HStack>
-                        <Text fontSize="sm">返信先：</Text>
-                        <Link href="/" color="blue.500">
-                          @{post.user.name}
-                        </Link>
-                        <Text fontSize="sm">さん</Text>
-                      </HStack>
-                    )}
+    currentUser && (
+      <Modal {...disclosure} colorScheme="twitter">
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleSubmit(handleReply)}>
+            <ModalHeader fontSize="25px" color="white" ml={0}>
+              <HStack justifyContent="start">
+                <IconButton
+                  icon={<CloseIcon />}
+                  size="sm"
+                  color="gray"
+                  rounded={100}
+                  aria-label="close-button"
+                  onClick={disclosure.onClose}
+                />
+              </HStack>
+            </ModalHeader>
+            <ModalBody pb={6}>
+              <Stack>
+                <HStack alignItems="start">
+                  <Stack>
+                    {post.user && <UserIcon name={post.user.name} />}
+                    <Center h="50px">
+                      <Divider
+                        orientation="vertical"
+                        size="lg"
+                        colorScheme="twitter"
+                      />
+                    </Center>
                   </Stack>
-                </Stack>
-              </HStack>
-              <HStack alignItems="start">
-                <Stack>{post.user && <UserIcon name={post.user.name} />}</Stack>
-                <FormControl>
-                  <Textarea
-                    variant="unstyled"
-                    placeholder="返信を投稿"
-                    size="md"
-                    color={colorMode === 'dark' ? 'white' : 'black'}
-                    fontSize="lg"
-                    resize="none"
-                    p={3}
-                    {...register('content', { required: true })}
-                  />
-                </FormControl>
-              </HStack>
-            </Stack>
-          </ModalBody>
+                  <Stack>
+                    <Stack px={1} fontSize="sm">
+                      <Text fontWeight="semibold" fontSize="md">
+                        {post.user?.name}
+                      </Text>
+                      <Text fontSize="sm">{post.body}</Text>
+                      {post.user && (
+                        <HStack>
+                          <Text fontSize="sm">返信先：</Text>
+                          <Link href="/" color="blue.500">
+                            @{post.user.name}
+                          </Link>
+                          <Text fontSize="sm">さん</Text>
+                        </HStack>
+                      )}
+                    </Stack>
+                  </Stack>
+                </HStack>
+                <HStack alignItems="start">
+                  <Stack>
+                    <UserIcon name={currentUser.name} />
+                  </Stack>
+                  <FormControl>
+                    <Textarea
+                      variant="unstyled"
+                      placeholder="返信を投稿"
+                      size="md"
+                      color={colorMode === 'dark' ? 'white' : 'black'}
+                      fontSize="lg"
+                      resize="none"
+                      p={3}
+                      {...register('content', { required: true })}
+                    />
+                  </FormControl>
+                </HStack>
+              </Stack>
+            </ModalBody>
 
-          <ModalFooter>
-            <Button
-              size="sm"
-              colorScheme="twitter"
-              type="submit"
-              rounded={50}
-              px={4}
-              isLoading={loading}
-              isDisabled={!isDirty || !isValid}
-            >
-              返信
-            </Button>
-          </ModalFooter>
-        </form>
-      </ModalContent>
-    </Modal>
+            <ModalFooter>
+              <Button
+                size="sm"
+                colorScheme="twitter"
+                type="submit"
+                rounded={50}
+                px={4}
+                isLoading={loading}
+                isDisabled={!isDirty || !isValid}
+              >
+                返信
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    )
   );
 }
